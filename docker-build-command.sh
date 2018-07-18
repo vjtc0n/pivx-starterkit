2157360a7e8c2e23238becc7e93ffd159fd9b0dd
vjtc0n <vjtc0n12345@gmail.com>
2018-07-18 21:35:10 +0700

    Fix typo

--- docker-build-command.sh
+++ docker-build-command.sh
@@ -3,4 +3,4 @@ set -e
 # version number argument 3.1.0.2
 VERSION="3.1.0.2"
 
-docker build -f Dockerfile --build-arg VERSION="${VERSION}" -t vjtc0n/pivx:"latest" .
+docker build -f Dockerfile --build-arg VERSION="${VERSION}" -t vjtc0n/pivx:latest .
